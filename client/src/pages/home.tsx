73be715809671927ea7ec8cc0a67616304c83fbd
hgfheue <40070674-hgfheue@users.noreply.replit.com>
2025-02-27 20:09:58 +0000

    Remove Telegram initialization from Home page
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/3d1e2d79-1515-4613-8cab-04b6a9cec12a/37f8a8bd-bca5-470c-8b51-37718df5b76b.jpg

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,13 +1,7 @@
-import { useEffect } from "react";
 import { UserMatrix } from "@/components/matrix/user-matrix";
-import { initTelegram } from "@/lib/telegram";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function Home() {
-  useEffect(() => {
-    initTelegram();
-  }, []);
-
   return (
     <div className="container py-4 max-w-5xl mx-auto">
       <Card>
